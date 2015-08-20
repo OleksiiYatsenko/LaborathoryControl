900abb48147a47e332da5fd53f367666f5abd607
OleksiiYatsenko <forsir2000@gmail.com>
2015-08-20 19:38:05 +0300

    Separate Logic of working with another documents

---
+++ LaborathoryControl/Model/OpenOfficeWorker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaborathoryControl.Model
+{
+    public class OpenOfficeWorker
+    {
+        public static void WorkWithOpenOffice()
+        {
+
+        }
+    }
+}
